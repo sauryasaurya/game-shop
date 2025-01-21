1622d2984503eca49b97314437e5a4a68cf5ac8f
Surya Bhandari <saurya.bhandari78792@gmail.com>
2025-01-21 15:29:10 +0545

    Added Chakra UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+});
+
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// })
