990baa2ecc621987901052815ae75594a8f7ce1a
Surya Bhandari <saurya.bhandari78792@gmail.com>
2025-02-25 11:35:20 +0545

    Cleanup the Game Cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -15,13 +15,13 @@ const GameCard = ({ game }: Props) => {
       <Card.Root>
         <Image src={getCroppedImageUrl(game.background_image)} />
         <Card.Body>
-          <Card.Title>{game.name}</Card.Title>
-          <HStack justifyContent="space-between">
+          <HStack justifyContent="space-between" marginBottom={2}>
             <PlatformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
             />
             <CriticScore score={game.metacritic} />
           </HStack>
+          <Card.Title>{game.name}</Card.Title>
         </Card.Body>
       </Card.Root>
     </GameCardContainer>
